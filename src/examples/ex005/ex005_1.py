c374db0bb886b4d20bbc14cbe65839604e2debb8
Tiago Sousa <tiagosousa.tams@hotmail.com>
2026-02-27 09:36:56 +0000

    implement tool calls

--- src/examples/ex005/ex005_1.py
+++ src/examples/ex005/ex005_1.py
@@ -1,7 +1,8 @@
 from typing import List
-from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage, ToolMessage
 from langchain_core.tools import BaseTool, tool
 from langchain.chat_models import init_chat_model
+from pydantic import ValidationError
 from rich import print
 
 #1 criar as ferramentas
@@ -22,7 +23,26 @@ messages: List[BaseMessage] = [system_message, human_message]
 
 
 tools: List[BaseTool] = [multiply]
+tools_by_name = {tool.name: tool for tool in tools}
 llm_with_tools = llm.bind_tools(tools)
 
-result = llm_with_tools.invoke(messages)
-print(result)
+llm_response = llm_with_tools.invoke(messages)
+messages.append(llm_response)
+
+if isinstance(llm_response, AIMessage) and getattr(llm_response, "tool_calls", None):
+    call = llm_response.tool_calls[-1]
+    name, args, id_ = call["name"], call["args"], call["id"]
+
+    try:
+        content = tools_by_name[name].invoke(args)
+        status = "success"
+    except (KeyError, IndexError, TypeError, ValidationError, ValueError) as error:
+        content = f"Please, fix your mistakes: {error}"
+        status = "error"
+
+
+    llm_response = llm_with_tools.invoke(messages)
+    messages.append(llm_response)
+
+
+print(messages)
